a88b906066f672b9e64bd3ad7d5c23fb7a0e8ca1
Developer23454 <breadboardwebsite@gmail.com>
2025-08-07 23:45:14 +0200

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,23 +7,23 @@ function createHeader() {
   head.appendChild(title);
 
   const icons = [
-    { rel: "icon", href: "/assets/images/logo/refresh-black.svg" },
+    { rel: "icon", href: "assets/images/logo/refresh-black.svg" },
     {
       rel: "icon",
       type: "image/png",
       sizes: "32x32",
-      href: "/assets/images/logo/refresh-black.svg",
+      href: "assets/images/logo/refresh-black.svg",
     },
     {
       rel: "icon",
       type: "image/png",
       sizes: "16x16",
-      href: "/assets/images/logo/refresh-black.svg",
+      href: "assets/images/logo/refresh-black.svg",
     },
     {
       rel: "apple-touch-icon",
       sizes: "180x180",
-      href: "/assets/images/logo/refresh-black.svg",
+      href: "assets/images/logo/refresh-black.svg",
     },
   ];
 
@@ -51,4 +51,5 @@ function createHeader() {
 }
 
 document.addEventListener("DOMContentLoaded", createHeader);
+
 
